ea2636a759b36a889676ce48cf144a9ece5773f6
bryant tunbutr <bryantt23@users.noreply.github.com>
2019-04-01 21:14:53 -0700

    Can send json from express
    https://flaviocopes.com/express-send-json-response/

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
+// https://flaviocopes.com/express-send-json-response/
+app.get('/data', (req, res) => res.json({ message: 'Hello world' })
+)
 //assuming app is express Object.
 app.get('/',function(req,res) {
     res.sendFile(__dirname + '/index.html');
 });
+
 
 app.listen(3000);
